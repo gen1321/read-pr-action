c28d34d6eff5236d70d5326a791482582220f922
gen <gen1321@mail.ru>
2020-03-16 01:32:57 +0300

    encode base 64

--- action.rb
+++ action.rb
@@ -3,9 +3,9 @@ require 'pry'
 require 'pry-doc'
 require 'rest-client'
 require 'octokit'
+require 'base64'
 
 client = Octokit::Client.new(:access_token => ARGV[0])
 resp = client.pull_request(ENV["GITHUB_REPOSITORY"], ARGV[1])
-body = resp.body.gsub(/\s/, '')
-p body
-exec("echo ::set-output name=body::'#{body}'")
+body = resp.body.gsub(/\s/, ' ')
+exec("echo ::set-output name=body::'#{Base63.encode64(body)}'")
