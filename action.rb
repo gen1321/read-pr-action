dfc8cd8bec63db24878630abe90e755524284901
gen <gen1321@mail.ru>
2020-03-16 00:49:31 +0300

    dbg

--- action.rb
+++ action.rb
@@ -4,6 +4,8 @@ require 'pry-doc'
 require 'rest-client'
 require 'octokit'
 
+puts 'ARGUMENTS'
+puts ARGV
 client = Octokit::Client.new(:access_token => ARGV[0])
 puts ARGV[1]
 resp = client.pull_request(ENV["GITHUB_REPOSITORY"], ARGV[1])
