da9da04c7219c7659e44e936d88d28cc541d77a4
gen <gen1321@mail.ru>
2020-03-16 01:16:27 +0300

    fix

--- action.rb
+++ action.rb
@@ -4,10 +4,6 @@ require 'pry-doc'
 require 'rest-client'
 require 'octokit'
 
-puts 'ARGUMENTS'
-puts ARGV
 client = Octokit::Client.new(:access_token => ARGV[0])
-puts ARGV[1]
 resp = client.pull_request(ENV["GITHUB_REPOSITORY"], ARGV[1])
-
-exec("echo ::set-output name=body::'#{resp.body}'")
+exec("echo ::set-output name=body::'#{resp.body.tr("\n", " ")}'")
