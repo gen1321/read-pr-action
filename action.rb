e243a458659df54e180372324887d0c7b72752a5
gen <gen1321@mail.ru>
2020-03-15 23:17:50 +0300

    dbg

--- action.rb
+++ action.rb
@@ -5,6 +5,7 @@ require 'rest-client'
 require 'octokit'
 
 client = Octokit::Client.new(:access_token => ARGV[0])
+PUTS ARGV[1]
 resp = client.pull_request(ENV["GITHUB_REPOSITORY"], ARGV[1])
 
 exec("echo ::set-output name=body::#{resp.body}")
