88f1e00a10846de5aac69682d7fd257a528ce810
gen <gen1321@mail.ru>
2020-03-16 01:28:53 +0300

    test

--- action.rb
+++ action.rb
@@ -6,6 +6,6 @@ require 'octokit'
 
 client = Octokit::Client.new(:access_token => ARGV[0])
 resp = client.pull_request(ENV["GITHUB_REPOSITORY"], ARGV[1])
-body = resp.body.gsub(/\s/, ' ')
+body = resp.body.gsub(/\s/, '')
 p body
 exec("echo ::set-output name=body::'#{body}'")
